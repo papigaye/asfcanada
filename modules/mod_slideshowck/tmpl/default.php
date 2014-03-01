2ef1798fd4b1dae0157447db2d38c097f5f0b69a
Moussa Thimbo <moussa.thimbo@gmail.com>
2014-03-01 02:43:59 -0500

    changement de la bande du slider

--- modules/mod_slideshowck/tmpl/default.php
+++ modules/mod_slideshowck/tmpl/default.php
@@ -54,10 +54,13 @@ $width = ($params->get('width') AND $params->get('width') != 'auto') ? ' style="
 			}
 			if (($item->imgcaption || $item->article) && (($params->get('lightboxcaption', 'caption') != 'title' || $imgtarget != 'lightbox') || !$item->imglink)) {
 				echo '<div class="camera_caption ' . $params->get('captioneffect', 'moveFromBottom') . '">';
+                               
+				echo '</div>';
+                                echo '<div class="camera_caption suite">';
                                 echo '<span>';
 				echo $datacaption;
                                 echo '<span>';
-				echo '</div>';
+                                echo '</div>';
 			}
 			?>
 		</div>
