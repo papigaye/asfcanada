4d874b8ee5b6c2799f583af6b4368d5836f28ae9
Moussa Thimbo <moussa.thimbo@gmail.com>
2014-03-21 19:57:24 -0400

    ajout module article preview et design

---
+++ modules/mod_WeSeKReadMore/tmpl/default.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * $mod_WeSeKReadMore
+ * 
+ * @version	1.0
+ * @package	modules
+ * @copyright	Copyright (C) Dec 2013 www.wesek.de All rights reserved.
+ * @license	GNU General Public License version 2
+ * -------------------------------------
+ */
+defined( '_JEXEC' ) or die;
+$document = JFactory::getDocument();
+
+$title 					= $params->get('title', 'Title');
+$description 			= $params->get('description', 'Description');
+$Directory 				= $params->get('Directory', '');
+$link 					= $params->get('link', 'Link');
+$picH 					= $params->get('pxHeight', 0);
+$picW 					= $params->get('pxWidth', 0);
+$textcolor 				= $params->get('textcolor', 0);
+$bgcolor 				= $params->get('bgcolor', 0);
+$WeSeK_moduleid         = $module->id ;
+$readMore				= $params->get('readMore', 'ReadMore');
+?>
+<div class="wesekReadMore<?php echo $WeSeK_moduleid; ?>">
+	<div class="wesekReadMore">
+		<div class="readMore">                   
+			<div class="mask">
+			<div class="news_img"><div class="jaune_trans"></div><img src="<?php echo $Directory; ?>"/></div>
+				<a href="<?php echo $link; ?>" class="info"><h2 class="news_h2"><?php echo $title; ?></a></h2>
+				<div class="news_desc"><?php echo $description; ?></div>
+			</div>
+		</div>  
+	</div>
+</div>
+
+
